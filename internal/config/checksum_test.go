b5b9506e913b1643b3fc295f9fa9b2905d1c5190
Ryan Ling <ryan@outlook.com.au>
2019-07-18 20:07:37 +1000

    chore: Extend checksum regression tests

--- internal/config/checksum_test.go
+++ internal/config/checksum_test.go
@@ -9,22 +9,109 @@ import (
 )
 
 func Test_CalculateChecksum(t *testing.T) {
+	stackInput := func(stack *config.Stack) func() interface{} {
+		return func() interface{} {
+			return stack.Hashable()
+		}
+	}
+
 	testCases := []struct {
 		description string
 		input       func() interface{}
 		expected    string
 	}{
 		{
-			description: "config.Stack.Hashable",
-			input: func() interface{} {
-				stack := &config.Stack{
-					Policy:   []byte("a"),
-					Template: []byte("b"),
-				}
+			description: "full config.Stack with checksum fields",
+			input: stackInput(&config.Stack{
+				Name:         "a",
+				Capabilities: []string{"b1", "b2"},
+				Parameters: config.StackParameters{
+					{
+						Key:   "c1",
+						Value: "c2",
+					},
+					{
+						Key:   "c3",
+						Value: "c4",
+					},
+				},
+				Tags: config.StackTags{
+					{
+						Key:   "d1",
+						Value: "d2",
+					},
+					{
+						Key:   "d3",
+						Value: "d4",
+					},
+				},
+				TerminationProtection: true,
 
-				return stack.Hashable()
-			},
-			expected: "5dc046d563a19c16dfae96d8b530873f7b6a758af3e68bde7aefa3d96790770c",
+				Policy:   []byte("e"),
+				Template: []byte("f"),
+
+				ArtefactBucket: "g",
+				PolicyKey:      "h",
+				TemplateKey:    "i",
+
+				Checksum: "j",
+			}),
+			expected: "4d76060a71bb68af4c917a48f3a09298ba9a7cab8f8890be39cc867b4f20645f",
+		},
+
+		{
+			description: "full config.Stack without checksum fields",
+			input: stackInput(&config.Stack{
+				Name:         "a",
+				Capabilities: []string{"b1", "b2"},
+				Parameters: config.StackParameters{
+					{
+						Key:   "c1",
+						Value: "c2",
+					},
+					{
+						Key:   "c3",
+						Value: "c4",
+					}},
+				Tags: config.StackTags{
+					{
+						Key:   "d1",
+						Value: "d2",
+					},
+					{
+						Key:   "d3",
+						Value: "d4",
+					},
+				},
+				TerminationProtection: true,
+
+				Policy:   []byte("e"),
+				Template: []byte("f"),
+
+				ArtefactBucket: "g",
+			}),
+			expected: "4d76060a71bb68af4c917a48f3a09298ba9a7cab8f8890be39cc867b4f20645f",
+		},
+		{
+			description: "minimal config.Stack",
+			input: stackInput(&config.Stack{
+				Name:                  "a",
+				Capabilities:          make([]string, 0),
+				Parameters:            make(config.StackParameters, 0),
+				Tags:                  make(config.StackTags, 0),
+				TerminationProtection: false,
+
+				Policy:   []byte("e"),
+				Template: []byte("f"),
+
+				ArtefactBucket: "",
+			}),
+			expected: "e5fcf031009c4780d9993fa6afcee53dd1079a513b9033176ffa8f61f148fc4d",
+		},
+		{
+			description: "nil config.Stack",
+			input:       stackInput(new(config.Stack)),
+			expected:    "1245e875630639894025da5af9f4bc530eda6e824b84c362ac190b4cca141d0a",
 		},
 		{
 			description: "map",
